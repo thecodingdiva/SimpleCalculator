f0be8adae41d30d4a9c2db5f40d21b2a25af2bd1
Joanne <thecodingdiva@gmail.com>
2018-04-16 11:00:35 -0400

    added calculatorEngine class with the calculate method that performs 4 basic operations

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class CalculatorEngine
+    {
+        public double Calculate(string operation, double Input1, double Input2)
+        {
+            double result;
+
+            switch (operation.ToLower())
+            {
+                //regardless of 'add' or '+' it will perform the calculation for result
+                case "add":
+                case "+":
+                    result = Input1 + Input2;
+                    break;
+
+                case "subtract":
+                case "-":
+                    result = Input1 - Input2;
+                    break;
+
+                case "multiply":
+                case "*":
+                    result = Input1 * Input2;
+                    break;
+
+                case "divide":
+                case "/":
+                    result = Input1 / Input2;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Specified opeartion is not recognized");
+            }
+            return result;
+        }
+    }
+}
