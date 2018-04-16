dc35a63bf6f14101410e8c3e3f4bb30f9e82847b
Joanne <thecodingdiva@gmail.com>
2018-04-16 11:05:00 -0400

    added exception handling in main

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,22 +10,29 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            //This is the object that is going to convert the string input to a number
-            InputConverter inputConverter = new InputConverter();
+            try
+            {
+                //This is the object that is going to convert the string input to a number
+                InputConverter inputConverter = new InputConverter();
 
-            //This is the engine that will perform the calculation
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
+                //This is the engine that will perform the calculation
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
 
 
-            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-            string operation = Console.ReadLine();
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
 
-            /*result is the number after the calculation has been completed.
-            Calculate is the method from the CalculatorEngine. 
-            This method will take the 3 inputs and output the result*/
-            double result = calculatorEngine.Calculate();
-            Console.WriteLine(result);
+                /*result is the number after the calculation has been completed.
+                Calculate is the method from the CalculatorEngine. 
+                This method will take the 3 inputs and output the result*/
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
